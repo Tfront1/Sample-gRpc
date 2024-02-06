5502b2420dfa46a83d3567f5cecb5ae9c9615afc
Taranko Taras <taras.taranko@intenselab.com>
2024-02-06 17:30:30 +0200

    Implemented client

--- Sample-gRpc/Sample-gRpc/Program.cs
+++ Sample-gRpc/Sample-gRpc/Program.cs
@@ -18,6 +18,7 @@ namespace Sample_gRpc
 
             // Configure the HTTP request pipeline.
             app.MapGrpcService<GreeterService>();
+            app.MapGrpcService<SampleService>();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
--- Sample-gRpc/gRpcClient/Program.cs
+++ Sample-gRpc/gRpcClient/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace gRpcClient
+﻿using Grpc.Net.Client;
+using SamplegRpc;
+
+namespace gRpcClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var channel = GrpcChannel.ForAddress("http://localhost:5195");
+
+            var client = new Sample.SampleClient(channel);
+
+            //var responce = client.GetFullName(new SampleRequest { FirstName = "Taras", LastName = "Taranko" });
+            //OR
+            var responce = await client.GetFullNameAsync(new SampleRequest { FirstName = "Taras", LastName = "Taranko" });
+
+            Console.WriteLine(responce.FullName);
+
+            Console.ReadLine();
         }
     }
 }
