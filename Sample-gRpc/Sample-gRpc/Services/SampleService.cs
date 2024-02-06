2ada56b9557b390598255fa49092e434e759bb2b
Taranko Taras <taras.taranko@intenselab.com>
2024-02-06 17:09:20 +0200

    Aded service

---
+++ Sample-gRpc/Sample-gRpc/Services/SampleService.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+using SamplegRpc;
+
+namespace Sample_gRpc.Services
+{
+    public class SampleService: Sample.SampleBase
+    {
+        public override async Task<SampleResponce> GetFullName(SampleRequest request, ServerCallContext context)
+        {
+            var result = $"{request.FirstName} {request.LastName}";
+            return new SampleResponce { FullName = result };
+        }
+    }
+}
